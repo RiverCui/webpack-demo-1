fc0cffbb20c0470452237f69f55953597a202329
River <moonquakes7@gmail.com>
2021-06-02 02:58:28 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,4 +12,41 @@ module.exports = {
       template: 'src/assets/index.html'
     }),
   ],
+  module: {
+    rules: [
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            use: ["file-loader"],
+          },
+        {
+            test: /\.styl$/i,
+            loader: [
+              "style-loader",
+              "css-loader",
+              "stylus-loader",
+            ],
+          },
+        {
+            test: /\.less$/i,
+            loader: [
+              "style-loader",
+              "css-loader",
+              "less-loader",
+            ],
+          },
+        {
+            test: /\.scss$/i,
+            use: [
+                "style-loader",
+                "css-loader",
+                {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require("dart-sass")
+                    }
+                }
+            ]
+        },
+    ],
+  },
 };
